a4dd0760babddeee90e90276828cee872fe671f5
Jordan Crouse <jcrouse@codeaurora.org>
2020-09-05 13:04:15 -0700

    drm/msm: Add support for private address space instances
    Add support for allocating private address space instances. Targets that
    support per-context pagetables should implement their own function to
    allocate private address spaces.
    
    The default will return a pointer to the global address space.
    
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/gpu/drm/msm/msm_gpu.h
+++ drivers/gpu/drm/msm/msm_gpu.h
@@ -64,6 +64,8 @@ struct msm_gpu_funcs {
 	void (*gpu_set_freq)(struct msm_gpu *gpu, unsigned long freq);
 	struct msm_gem_address_space *(*create_address_space)
 		(struct msm_gpu *gpu, struct platform_device *pdev);
+	struct msm_gem_address_space *(*create_private_address_space)
+		(struct msm_gpu *gpu);
 };
 
 struct msm_gpu {
@@ -293,6 +295,9 @@ void msm_gpu_submit(struct msm_gpu *gpu, struct msm_gem_submit *submit);
 int msm_gpu_init(struct drm_device *drm, struct platform_device *pdev,
 		struct msm_gpu *gpu, const struct msm_gpu_funcs *funcs,
 		const char *name, struct msm_gpu_config *config);
+
+struct msm_gem_address_space *
+msm_gpu_create_private_address_space(struct msm_gpu *gpu);
 
 void msm_gpu_cleanup(struct msm_gpu *gpu);
 
